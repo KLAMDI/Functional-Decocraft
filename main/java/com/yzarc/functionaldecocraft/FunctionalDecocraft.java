30cc7ccb1ca2dba305a25a54e1d042b25b73d71d
KLAMDI <crunch@hetnet.nl>
2019-12-24 15:37:10 +0100

    Updated mod id

--- main/java/com/yzarc/functionaldecocraft/FunctionalDecocraft.java
+++ main/java/com/yzarc/functionaldecocraft/FunctionalDecocraft.java
@@ -8,7 +8,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = FunctionalDecocraft.MODID, version = FunctionalDecocraft.VERSION, dependencies = "required-after:(props)")
 public class FunctionalDecocraft
 {
-    public static final String MODID = "Functional Decocraft";
+    public static final String MODID = "funcocraft";
     public static final String VERSION = "0.1";
     
     @EventHandler
